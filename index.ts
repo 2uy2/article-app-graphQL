45a840065b373216ea8f15fe20daa5581d04bb49
2uy2 <quytran11204@gmail.com>
2026-01-27 17:48:27 +0700

    kết nối database

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import express, {Express, Request,Response} from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+import Article from "./models/article_model";
+dotenv.config();
+database.connect();
 
 const app: Express=express();
-const port :number=3000;
+const port :number|string =process.env.PORT;
 
 //rest api 
-app.get(`/articles`,(req:Request,res:Response)=>{
+app.get(`/articles`,async(req:Request,res:Response)=>{
+    const article = await Article.find({
+        deleted:false
+    });
+
     res.json({
-        articles:[]
+        articles:article
     });
 })
 
---
+++ models/article_model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const articleSchema = new mongoose.Schema (
+    {
+        title:String,
+        avatar:String,
+        description:String,
+        deleted:{
+            type:Boolean,
+            default:false
+        },
+        deletedAt:Date,
+        
+    },
+    {
+        timestamps:true
+    }
+);
+const Article = mongoose.model('Article',articleSchema,"article");
+//tham số đầu là tên để gọi dữ liệu, tham số hai là khung, tham số ba là bảng dữ liệu
+export default Article;
+
