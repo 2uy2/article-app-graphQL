8f83ebe51ef7d630d8c8f61174e6568040ef15a9
2uy2 <quytran11204@gmail.com>
2026-01-27 22:06:25 +0700

    lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,8 +1,16 @@
+import Article from "./models/article_model"
+
 //resolvers chọc vào database để lấy data nhưng tuân thủ thẻ Query 
 export const resolvers = {
     Query: {
         hello: () => {
             return "Hello World"
+        },
+        getListArticle:async()=>{
+            const article = await Article.find({
+                deleted:false
+            });
+            return article
         }
     }
 }
